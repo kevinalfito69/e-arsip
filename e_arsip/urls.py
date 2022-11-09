a9e453635948f34491e761e4ea5eecd789ff40d3
kevinalfito69 <kevinalfito69@gmail.com>
2022-11-09 13:55:20 +0700

    Update url

--- e_arsip/urls.py
+++ e_arsip/urls.py
@@ -18,26 +18,6 @@ from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path(
-        'admin/password_reset/',
-        auth_views.PasswordResetView.as_view(),
-        name='admin_password_reset',
-    ),
-    path(
-        'admin/password_reset/done/',
-        auth_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done',
-    ),
-    path(
-        'reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm',
-    ),
-    path(
-        'reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete',
-    ),
     path('admin/', admin.site.urls, name='admin'),
     path('', include('data_surat.urls', namespace='data_surat')),
 ]
